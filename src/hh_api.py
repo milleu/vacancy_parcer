ac59cad49f40be92ab40e6a69a3882171eb6c6f6
Daria Beliaeva <dariabeliaeva22@gmail.com>
2023-07-09 12:19:49 -0700

    classes for both websited added

--- src/hh_api.py
+++ src/hh_api.py
@@ -1,48 +1,54 @@
 import json
+from abc import ABC
 
 import requests
 
-class HeadHunter:
-    def __init__(self, keyword):
+from src.abstract_class import Job_Search
+
+
+class HeadHunter(Job_Search, ABC):
+    """класс, в котором осуществляется работа с платформой Head Hunter"""
+    def __init__(self, keyword, count):
+        self.count = count
         self.url = "https://api.hh.ru/vacancies"
         self.params = {
-            "per_page": 100,
+            "per_page": self.count,
             "page": None,
             "text": keyword,
-            "archived": False,
-        }
+            "archived": False}
         self.headers = {
              "User-Agent": "50355527"
         }
         self.vacancies = []
 
+
     def get_request(self):
+        """делаем реквест к апи"""
         response = requests.get(self.url, headers=self.headers, params=self.params)
         if response.status_code != 200:
             raise Exception(f"Ошибка получения вакансий! Статус: {response.status_code}")
         return response
 
     def get_vacancy(self):
+        """получаем список вакансий"""
         vacancy_list = []
         data = self.get_request().json()
         for item in data['items']:
             job_name = item['name']
             emp_name = item['employer']['name']
-            experience = item['experience']['name']
-            if item['salary']:
-                salary_min = item['salary']["from"]
-            else:
-                salary_min = "Нет"
-            if item['salary']:
-                salary_max = item['salary']['to']
-            else:
-                salary_max = "Нет"
+            description = item['snippet']['requirement'] if item['snippet'] and 'requirement' in item[
+                    'snippet'] else None
+            try:
+                if item['salary']:
+                    salary_min = item['salary']["from"]
+            except Exception:
+                    salary_min = None
+
 
             job = {"job_name": job_name,
                     "emp_name": emp_name,
-                    "experience": experience,
-                    "salary_min": salary_min,
-                    "salary_max": salary_max}
+                    "description": description,
+                    "salary_min": salary_min}
             vacancy_list.append(job)
 
         self.write_data_to_file(vacancy_list)
@@ -51,10 +57,8 @@ class HeadHunter:
 
 
     def write_data_to_file(self, jobs):
+        """записываем вакансии в файл"""
         with open("../src/hh_vacancy.json", "w") as file:
             json.dump(jobs, file, sort_keys=False, indent=4, ensure_ascii=False)
 
 
-
-h = HeadHunter("python")
-print(h.get_vacancy())
